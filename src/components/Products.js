a33bbeb942c4613ae4f3b1f35f3bc10d7f880c33
akshara-aravind <aksharareddydilli1001@gmail.com>
2023-01-03 10:58:34 +0530

    exercise-3

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,41 +1,51 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 import '../App.css'
-// import imge from '../images/product1.png'
-import {useQuery} from 'react-query'
+import { useQuery } from 'react-query'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
-const fetchproductdata= (id)=>{
-        console.log("usequery is called")
-        return  axios.get(`http://localhost:4000/products/${id}`)
+const fetchproductdata = (id) => {
+    console.log("usequery is called")
+    return axios.get(`http://localhost:4000/products/${id}`)
 }
-export const Products=()=>{
-    const {id}=useParams();
+export const Products = () => {
+    const { id } = useParams();
     console.log(id)
-    const {data,isLoading,isSuccess}=useQuery('productsItems2',()=>fetchproductdata(id));
+    const { data, isLoading, isSuccess} = useQuery('productsItems2', () => fetchproductdata(id));
     // useEffect(()=>{
     //     console.log(data)
     // },[data])
-    if(isLoading)
-    {
+    if (isLoading) {
         console.log("Loading")
     }
-    if(data&&isSuccess)
-    {
-        console.log("Data is here:",data);
+    if(isSuccess){
+        console.log('data is here'+data);
     }
+   
+    
     return (
-        <div className='ProductPage'>
-        <div className='ProductImage'><img src={data?.data.image} alt='product'/>
-        </div>
-        <div className='ProductDescription'>
-        <h1>{data?.data.name}</h1>
-        <div >{data?.data.name} Description</div><br/>
-        <div>Price:<b>${data?.data.price}</b></div><br/>
-        <div>quantity:{data?.data.quantity}</div><br/>
-        <button className='AddToCartBtn'>Add to cart</button>
-        </div>
-        </div>
-       );
-}
-
+        
+            
+            <div className='ProductPage'>
+                <div className='ProductImage'><img src={data && data.data.image} alt='product' />
+                </div>
+                <div className='ProductDescription'>
+                    <h1>{data && data.data.name}</h1>
+                    <div >{data && data.data.name} Description</div><br />
+                    <div>Price:<b>${data && data.data.price}</b></div><br />
+                    <div>quantity:{data && data.data.quantity}</div><br />
+                    <div>quantity:{data && data.data.quantity > 10 && <i>Available</i>}
+                        {data && data.data.quantity <= 10 && data && data.data.quantity > 0 && <i>selling fast</i>}
+                        {data && data.data.quantity === 0 && <i>unavailable</i>}</div><br />
+                    <div className='variant'>variants:
+                        {data && data.data.variants.map((product) => {
+                            return <div key={product.color} ><button >{product.color}</button></div>
+                        })}
+                    </div><br />
+                    <button className='AddToCartBtn'>Add to cart</button>
+                </div>
+            </div>
+       
+    );
+                    }
