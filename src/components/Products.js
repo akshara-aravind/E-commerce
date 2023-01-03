35764473e186b94ddfc4f92055b0b53c64d7073c
akshara-aravind <aksharareddydilli1001@gmail.com>
2023-01-03 12:25:08 +0530

    exercise-3

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useState,useEffect} from 'react'
 import '../App.css'
 import { useQuery } from 'react-query'
 import axios from 'axios'
@@ -11,36 +11,37 @@ const fetchproductdata = (id) => {
 }
 export const Products = () => {
     const { id } = useParams();
+    const [show,setShow]=useState(null);
     console.log(id)
-    const { data, isLoading, isSuccess} = useQuery('productsItems2', () => fetchproductdata(id));
-    // useEffect(()=>{
-    //     console.log(data)
-    // },[data])
+    const { data, isLoading, isSuccess,isFetched} = useQuery('productsItems', () => fetchproductdata(id));
+    useEffect(() => {
+        setShow(data?.data.image)
+        console.log("show image is set",data?.data.image)
+    }, [data])
     if (isLoading) {
         console.log("Loading")
     }
-    if(isSuccess){
-        console.log('data is here'+data);
+    if(data&&isSuccess){
+        console.log('data is here',data);
     }
-   
-    
     return (
         
             
             <div className='ProductPage'>
-                <div className='ProductImage'><img src={data && data.data.image} alt='product' />
+                <div className='ProductImage'><img src={show} alt='product' />
                 </div>
                 <div className='ProductDescription'>
                     <h1>{data && data.data.name}</h1>
                     <div >{data && data.data.name} Description</div><br />
                     <div>Price:<b>${data && data.data.price}</b></div><br />
-                    <div>quantity:{data && data.data.quantity}</div><br />
                     <div>quantity:{data && data.data.quantity > 10 && <i>Available</i>}
                         {data && data.data.quantity <= 10 && data && data.data.quantity > 0 && <i>selling fast</i>}
                         {data && data.data.quantity === 0 && <i>unavailable</i>}</div><br />
                     <div className='variant'>variants:
                         {data && data.data.variants.map((product) => {
-                            return <div key={product.color} ><button >{product.color}</button></div>
+                            return <div className='variantImage' key={product.color} ><button onClick={()=>{
+                                setShow(product.image)
+                            }}>{product.color}</button></div>
                         })}
                     </div><br />
                     <button className='AddToCartBtn'>Add to cart</button>
